58ad0fccfb3708f3081baa5712c09d67df081b41
Alaeddine <87445066+alaeddineazri@users.noreply.github.com>
2021-10-15 19:09:13 +0100

    Add files via upload

---
+++ DML-Checkpoint.sql
@@ -0,0 +1,4 @@
+INSERT INTO Products VALUES (‘P01’,’Samsung Galaxy S20’,’Smartphone’,3299); 
+INSERT INTO Products VALUES (‘P02’,’Asus Notebook’,’PC’,4599);
+INSERT INTO Customer VALUES (‘C01’,’Ali’,71321009); 
+INSERT INTO Customer VALUES 
